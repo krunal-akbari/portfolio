79227fcc63579f689801b84f47c93ed99cedcc1a
krunal233 <krunal@nalashaa.com>
2025-02-24 17:15:24 +0530

    init commit

---
+++ app/layout.js
@@ -0,0 +1,37 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Krunal",
+  description: "This is a app where you can contact me",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="This is a app where you can contact me" />
+        <meta name="keywords" content="Krunal, Contact, Form, App" />
+        <meta name="author" content="Krunal" />
+        <meta name="robots" content="index, follow" />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
