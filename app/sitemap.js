5169f8ed00443bf5b8325b060dec25041792399b
krunal-akbari <kruanlakbari233@gmail.com>
2025-02-26 22:51:35 +0530

    feat: adding Sitemap
    - adding sitemap which has been generated currently and will generate
      when new build happen

---
+++ app/sitemap.js
@@ -0,0 +1,34 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://krunalakbari.in',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: 'https://krunalakbari.in/about',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8,
+    },
+    {
+      url: 'https://krunalakbari.in/contact',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.5,
+    },
+    {
+      url: 'https://krunalakbari.in/projects',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.5,
+    },
+    {
+      url: 'https://krunalakbari.in/pricing',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.5,
+    },
+  ]
+}
