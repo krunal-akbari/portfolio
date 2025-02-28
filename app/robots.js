0b22aa43484dbe4ed0b7df3b7bdca00e0650b037
krunal-akbari <kruanlakbari233@gmail.com>
2025-02-28 23:34:56 +0530

    Adding robots and sitemap
    - adding sitemap for more enhance google crowling
    - adding robots for more enhance google crowling

--- app/robots.js
+++ app/robots.js
@@ -1,9 +1,11 @@
 export default function robots() {
     return {
-      rules: {
-        userAgent: '*',
-        allow: '/',
-        disallow: '/private/',
-      },
-    }
-  }
+        rules: {
+            userAgent: "*",
+            allow: "/",
+            disallow: "/private/",
+        },
+        sitemap: "https://acme.com/sitemap.xml",
+    };
+}
+
--- app/sitemap.js
+++ app/sitemap.js
@@ -1,34 +1,52 @@
 export default function sitemap() {
-  return [
-    {
-      url: 'https://krunalakbari.in',
-      lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 1,
-    },
-    {
-      url: 'https://krunalakbari.in/about',
-      lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.8,
-    },
-    {
-      url: 'https://krunalakbari.in/contact',
-      lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.5,
-    },
-    {
-      url: 'https://krunalakbari.in/projects',
-      lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.5,
-    },
-    {
-      url: 'https://krunalakbari.in/pricing',
-      lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.5,
-    },
-  ]
+    return [
+        {
+            url: "https://krunalakbari.in",
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 1,
+        },
+        {
+            url: "https://krunalakbari.in/about",
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.8,
+        },
+        {
+            url: "https://krunalakbari.in/contact",
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.5,
+        },
+        {
+            url: "https://krunalakbari.in/projects",
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.5,
+        },
+        {
+            url: "https://krunalakbari.in/pricing",
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.5,
+        },
+        {
+            url: "https://www.krunalakbari.in/process",
+            lastmodified: new date(),
+            changefrequency: "weekly",
+            priority: 0.5,
+        },
+        {
+            url: "https://www.krunalakbari.in/faqs",
+            lastmodified: new date(),
+            changefrequency: "weekly",
+            priority: 0.5,
+        },
+        {
+            url: "https://www.krunalakbari.in/projects/1",
+            lastmodified: new date(),
+            changefrequency: "weekly",
+            priority: 0.5,
+        },
+    ];
 }
