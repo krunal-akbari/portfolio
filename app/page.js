fc7aa77ce1de02dcb75c50b3c3f8a6f029f64a13
krunal-akbari <kruanlakbari233@gmail.com>
2025-03-03 21:19:00 +0530

    quick Fix on Prod. Env
    - whlie entaring metadata used wrong formate and not comiled

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
+export const metadata = {
     title: "Krunal Akbari | Digital Design Studio & Branding Expert",
     description: "Elevate your brand with innovative digital design & strategy. Krunal Akbari specializes in UX/UI, web development, brand identity, and digital marketing.",
+    keywords: "Ui/Ux development",
 };
 
-import  HomePageComponent  from "@/components/HomePageComponent";
+import HomePageComponent from "@/components/HomePageComponent";
 export default function HomePage() {
     return <HomePageComponent />;
 }
